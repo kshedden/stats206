a4ab4772d2b31c658103d45014c57d74f51b7d42
Kerby Shedden <kshedden@umich.edu>
2021-09-02 11:50:06 -0400

    update

--- case_studies/download.sh
+++ case_studies/download.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo Warning: running this script will overwrite your existing notebooks for Statistics 206.
+echo Exit now by typing ctrl-c to prevent this from happening.
+echo Press any other key to continue.
+read
+
 nb=(scooters_basic acs_basic who_mort nhanes_sampling ghcn_scatterplot)
 
 for v in ${nb[@]}; do 
