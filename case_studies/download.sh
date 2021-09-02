5256f1943efea5281f432555c5e101f07fbb0b03
Kerby Shedden <kshedden@umich.edu>
2021-09-02 10:53:24 -0400

    update

--- case_studies/download.sh
+++ case_studies/download.sh
@@ -3,6 +3,6 @@
 nb=(scooters_basic acs_basic who_mort nhanes_sampling ghcn_scatterplot)
 
 for v in ${nb[@]}; do 
-    wget raw.githubusercontent.com/kshedden/stats206/main/case_studies/${v}.ipynb
+    wget raw.githubusercontent.com/kshedden/stats206/main/case_studies/${v}.ipynb -O ${v}.ipynb
 done
 
